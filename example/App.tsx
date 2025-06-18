9f5917af5a4cfe7f9bec0fc00b57ce2fb20b18d5
Yiwei Gong <imwithye@gmail.com>
2025-06-18 18:51:23 +0800

    feat: add switch samples (#11)

--- example/App.tsx
+++ example/App.tsx
@@ -1,28 +1,100 @@
-import { createStaticNavigation } from "@react-navigation/native";
+import {
+  ActionSheetProvider,
+  useActionSheet,
+} from "@expo/react-native-action-sheet";
+import {
+  createStaticNavigation,
+  useNavigation,
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { Linking, ScrollView } from "react-native";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { ActivityIndicator, Button, Linking, ScrollView } from "react-native";
 import { Markdown } from "react-native-remark";
 
-const URL =
-  "https://raw.githubusercontent.com/imwithye/react-native-remark/refs/heads/main/markdown/05_article.md";
+const BASE_URL =
+  "https://raw.githubusercontent.com/imwithye/react-native-remark/refs/heads/main/markdown";
+const URL = `${BASE_URL}/01_markdown_basics.md`;
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  const { showActionSheetWithOptions } = useActionSheet();
+  const [url, setUrl] = useState(URL);
   const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          title="Load"
+          onPress={() => {
+            const options = [
+              {
+                title: "Cancel",
+                url: "",
+              },
+              {
+                title: "1. Markdown Basics",
+                url: `${BASE_URL}/01_markdown_basics.md`,
+              },
+              {
+                title: "2. Task List and Nested",
+                url: `${BASE_URL}/02_task_list_and_nested.md`,
+              },
+              {
+                title: "3. Table",
+                url: `${BASE_URL}/03_table_example.md`,
+              },
+              {
+                title: "4. Math and Code",
+                url: `${BASE_URL}/04_math_and_code.md`,
+              },
+              {
+                title: "5. Sample Article",
+                url: `${BASE_URL}/05_article.md`,
+              },
+              {
+                title: "6. PyTorch Tutorial",
+                url: `${BASE_URL}/06_pytorch.md`,
+              },
+            ];
+            const cancelButtonIndex = 0;
+            showActionSheetWithOptions(
+              {
+                options: options.map((option) => option.title),
+                cancelButtonIndex,
+              },
+              (idx?: number) => {
+                if (!idx || idx === cancelButtonIndex) return;
+                setUrl(options[idx].url);
+              },
+            );
+          }}
+        />
+      ),
+    });
+  }, [navigation, showActionSheetWithOptions, setUrl]);
 
   useEffect(() => {
-    fetch(URL)
+    setLoading(true);
+    fetch(url)
       .then((res) => res.text())
-      .then((text) => setMarkdown(text));
-  }, []);
+      .then((text) => setMarkdown(text))
+      .finally(() => setTimeout(() => setLoading(false), 1000));
+  }, [url]);
+
   return (
     <ScrollView style={{ flex: 1, padding: 8, backgroundColor: "white" }}>
       <StatusBar style="auto" />
-      <Markdown
-        markdown={markdown}
-        onLinkPress={(url) => Linking.openURL(url)}
-      />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Markdown
+          markdown={markdown}
+          onLinkPress={(url) => Linking.openURL(url)}
+        />
+      )}
     </ScrollView>
   );
 };
@@ -36,5 +108,9 @@ const RootStack = createNativeStackNavigator({
 const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <ActionSheetProvider>
+      <Navigation />
+    </ActionSheetProvider>
+  );
 }
